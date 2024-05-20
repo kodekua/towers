88dc479b8206697091101a07d090a7082700a046
gbazack <morenzack@gmail.com>
2024-05-20 20:40:52 +0200

    (#3) init preprocessing module

---
+++ models/preprocessing.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import csv
+import os
+
+
+class Extractor():
+    def __init__(self) -> None:
+        pass
+
+@classmethod
+def _extract(self, file_path:str, source="opencellid") -> pd.DataFrame:
+    extracted_df = pd.read_csv(file_path)
+    return
+
+@classmethod
+def _parse() -> DataFrame:
+    return
